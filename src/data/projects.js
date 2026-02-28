0dca438cf3ae5f6536229c34b5d6260114e971a9
c0r3dump <c0r3dump@MacBook-Air-4.local>
2026-02-28 14:24:12 +0800

    update: Add projects pages

---
+++ src/data/projects.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        name: "Boss Job Scraper",
+        description:
+            "一款Boss职位抓取工具",
+        github: "",
+        downloadUrl: "/downloads/boss-job-scraper.zip",
+        techStack: ["Javascript", "Chrome Ext", "Vibe Coding"],
+        date: "2026-02",
+    },
+];
